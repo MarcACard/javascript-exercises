f0957b10423cb600a808f0bc9932a62e3fc78ded
Marc Card <marcacard@gmail.com>
2021-04-04 14:34:17 -0700

    add some/every exercise solutions and readme for u8

---
+++ u8-some-every/README.md
@@ -0,0 +1,3 @@
+# Array Methods - some & every
+
+Exercises within this directory focus on practicing the some & every array methods. Completed exercises/solutions can be found in the [some-every.js](some-every.js) file.
--- u8-some-every/some-every.js
+++ u8-some-every/some-every.js
@@ -6,7 +6,7 @@ Examples:
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) { return arr.some(value => value % 2 !== 0) }
 
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
@@ -16,7 +16,7 @@ Examples:
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) { return num.toString().split('').some(value => value == 0) }
 
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
@@ -26,7 +26,7 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) { return arr.every(value => value % 2 !== 0) }
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -36,7 +36,7 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) { return arr.every(value => arr.indexOf(value) === arr.lastIndexOf(value)) }
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
@@ -53,7 +53,7 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) { return arr.every(obj => obj[key] !== undefined) }
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -71,4 +71,4 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) { return arr.every(obj => obj[key] === searchValue) }
